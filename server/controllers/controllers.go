7d80b3417e29a2784e4666703c37a93be6d69b01
jimmy <lettevoueto.jimmy@gmail.com>
2025-11-24 14:40:02 +0100

    ajouts des commentaires sur la configuration des routes

--- server/controllers/controllers.go
+++ server/controllers/controllers.go
@@ -5,9 +5,10 @@ import (
 	"net/http"
 )
 
+// configure toutes les routes de notre API.
 func Routes(){
 	
-	http.HandleFunc("/",handlers.Home)
-	http.HandleFunc("/health",handlers.Healt)
-	http.HandleFunc("/flight",handlers.Flight)
+	http.HandleFunc("/",handlers.Home) // Page d'accueil
+	http.HandleFunc("/health",handlers.Healt) // Vérifie si le serveur tourne bien
+	http.HandleFunc("/flight",handlers.FlightSorted) // Récupère tous les vols triés
 }
